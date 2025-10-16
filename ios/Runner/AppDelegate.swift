a8d9469650784dbffa7d83a0eb7150cf17d52a79
Charlotte <18631656747@163.com>
2025-10-16 17:21:15 +0800

    Remove workflow files

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import AppTrackingTransparency
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,12 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      DispatchQueue.main.asyncAfter(deadline: .now() + 4.95) {
+      if #available(iOS 14, *) {
+          ATTrackingManager.requestTrackingAuthorization { status in
+          }
+        }
+      }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
